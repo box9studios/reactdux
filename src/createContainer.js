611bd9f8f785eed11d58afc0a369f29b377bbedf
Steve Binder <email@stevebinder.com>
2018-10-22 22:44:38 -0400

    Update container logic

--- src/createContainer.js
+++ src/createContainer.js
@@ -1,11 +1,11 @@
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { getState } from './utils';
+import { isArray } from './utils';
 
 const defaultObj = {};
 const defaultMapper = () => defaultObj;
 
-const createMapper = value => {
+const createMapper = (value, comp) => {
   if (!value) {
     return defaultMapper;
   }
@@ -14,7 +14,7 @@ const createMapper = value => {
     : () => value;
   return connect(
     (state, props) => {
-      const mapped = mapper(props, getState()) || {};
+      const mapped = mapper(props, state, comp) || defaultObj;
       const copy = { ...mapped };
       for (const key in copy) {
         if (
@@ -31,11 +31,20 @@ const createMapper = value => {
 };
 
 export default function createContainer(...args) {
-  const component =  args[args.length - 1];
-  const mapper =  args[args.length - 2];
-  const hocs = args.slice(0, args.length - 2) || [];
+  const component = args[args.length - 1];
+  const mappers = [];
+  const hocs = [];
+  args
+    .slice(0, args.length - 1)
+    .forEach(arg => {
+      if (isArray(arg)) {
+        arg.forEach(a => hocs.push(a));
+      } else {
+        mappers.push(arg);
+      }
+    });
   return compose(
+    ...mappers.map(mapper => createMapper(mapper, component)),
     ...hocs,
-    createMapper(mapper),
   )(component);
 }
