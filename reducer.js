706c8ec5c34e3c36a145779c3e10931f40c6732c
Steve Binder <email@stevebinder.com>
2019-01-13 23:44:50 -0500

    Update reducer

--- reducer.js
+++ reducer.js
@@ -1,37 +1,66 @@
 import { isArguments } from './utils';
-import { combineReducers } from 'redux';
+
+const setPathValue = (state, { path, value }) => {
+  let copy = {};
+  let ref;
+  path.forEach((key, index) => {
+    if (index === 0) {
+      copy[key] = { ...state[key] };
+      if (path.length == 1) {
+        ref = copy;
+      } else {
+        ref = copy[key];
+      }
+    } else if (index < path.length - 1) {
+      ref[key] = { ...ref[key] };
+      ref = ref[key];
+    }
+  });
+  if (value === undefined) {
+    delete ref[path[path.length - 1]];
+  } else if (typeof value === 'function') {
+    ref[path[path.length - 1]] = value(ref[path[path.length - 1]]);
+  } else {
+    ref[path[path.length - 1]] = value;
+  }
+  return copy;
+};
+
+const getActors = (config, action) => {
+  const actors = config.reduce(
+    (result, args) => {
+      const types = [...args];
+      const handler = types.pop();
+      if (
+        types.find(type =>
+          type === action.type
+          || (type && type === action.__reactduxIdentity)
+        )
+      ) {
+        return [...result, handler];
+      }
+      return result;
+    },
+    [],
+  );
+  if (action.__reactduxActionPathValue) {
+    return [...actors, setPathValue];
+  }
+  return actors;
+};
 
 export default (defaultState = {}, config = []) => {
-  if (!config.length) {
-    return combineReducers(defaultState);
-  }
   const reducer = (state = defaultState, action) => {
-    const handlers = config.reduce(
-      (result, args) => {
-        const types = [...args];
-        const handler = types.pop();
-        if (
-          types.find(type =>
-            type === action.type
-            || (type && type === action.__reactduxIdentity)
-          )
-        ) {
-          return [...result, handler];
-        }
-        return result;
-      },
-      [],
-    );
-    return handlers.reduce(
+    const actors = getActors(config, action);
+    return actors.reduce(
       (prevState, handler) => {
         const changes = isArguments(action.payload)
           ? handler(state, ...action.payload)
           : handler(state, action.payload);
-        try {
-          return { ...prevState, ...changes };
-        } catch (error) {
-          return prevState;
-        }
+        return {
+          ...prevState,
+          ...changes,
+        };
       },
       { ...state },
     );
