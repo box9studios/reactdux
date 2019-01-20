ba0eea8a75c02034728205825f124ca8fe3d3b85
Steve Binder <email@stevebinder.com>
2019-01-20 16:00:14 -0500

    Update logger

--- logger.js
+++ logger.js
@@ -1,19 +1,48 @@
-import { getAction } from './utils';
-
-const DASHES = '--------------------------------------------';
+const DASHES = '----------------------';
 
 const getActionType = (action = {}, actionExports = {}) => {
-  const getExportAction = method => (Object.entries(actionExports)
-    .find(([key, value]) => value === method) || {})[0] || '';
-  const exportAction = getExportAction(action.type);
-  if (exportAction) {
-    return exportAction;
+  if (
+    typeof action.type === 'string'
+    && action.type !== 'ReactduxAction'
+  ) {
+    return action.type;
   }
-  if (action.__reactduxAction) {
-    const found = getExportAction(action.payload.method) || getExportAction(getAction());
-    return `${found} (${action.type})`;
+  const method = action.type === 'ReactduxAction'
+    ? action.payload.method
+    : action.type;
+  const entry = Object.entries(actionExports)
+    .find(([key, value]) => value === method);
+  return entry ? entry[0] : 'anonymous';
+};
+
+const getObjectDiff = (a = {}, b = {}) => {
+  const diff = {};
+  const checkPath = (c, d, ref) => {
+    const dCopy = d ? { ...d } : {};
+    for (const key in c) {
+      const cValue = c ? c[key] : c;
+      const dValue = d ? d[key] : d;
+      if (cValue !== dValue) {
+        if (
+          cValue
+          && typeof cValue === 'object'
+          && dValue
+          && typeof dValue === 'object'
+        ) {
+          ref[key] = {};
+          checkPath(cValue, dValue, ref[key]);
+        } else {
+          ref[key] = dValue;
+        }
+      }
+      delete dCopy[key];
+    }
+    for (const key in dCopy) {
+      ref[key] = dCopy[key];
+    }
   }
-  return action.type || 'AnonymousAction';
+  checkPath(a, b, diff);
+  return diff;
 };
 
 const log = target => {
@@ -29,17 +58,14 @@ export default (actionExports = {}) =>
   store =>
     next =>
       action => {
+        const before = store.getState();
+        next(action);
         console.groupCollapsed(`${DASHES}\nACTION: ${getActionType(action, actionExports)}\n${DASHES}`);
         console.groupCollapsed('payload:');
         log(action.payload);
         console.groupEnd();
-        next(action);
-        // console.groupCollapsed('diff:');
-        // log(store.getState());
-        // console.groupEnd();
-        // console.groupEnd();
-        // console.groupCollapsed('state:');
-        // log(store.getState());
-        // console.groupEnd();
-        // console.groupEnd();
+        console.groupCollapsed('diff:');
+        log(getObjectDiff(before, store.getState()));
+        console.groupEnd();
+        console.groupEnd();
       };
