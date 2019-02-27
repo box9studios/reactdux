1c768578598df75ac6d1dcb69ff2f55d858db367
Steve Binder <email@stevebinder.com>
2019-02-27 18:20:51 -0500

    Add context

---
+++ context.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from 'react';
+
+const isNonNullObject = value => value !== null && typeof value === 'object';
+
+export default (defaultState = null, actionConfig = {}) => {
+  const Context = createContext();
+  const Consumer = Context.Consumer;
+  const Provider = ({ children }) => {
+    const [state, setState] = useState(defaultState);
+    const stateTool = (...args) => setState(...args);
+    if (isNonNullObject(state)) {
+      Object.entries(state).forEach(([key, value]) => stateTool[key] = value);
+    }
+    const actions = Object.entries(actionConfig).reduce(
+      (result, [name, method]) => ({
+        ...result,
+        [name]: (...args) => method(stateTool, ...args),
+      }),
+      {},
+    );
+    const value = isNonNullObject(state) ? { ...state, ...actions } : state;
+    return (
+      <Context.Provider value={value}>
+        {children}
+      </Context.Provider>
+    );
+  };
+  return { Context, Consumer, Provider };
+};
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ export { default as action } from './action';
 export { default as app } from './app';
 export { default as component } from './component';
 export { default as container } from './container';
+export { default as context } from './context';
 export { default as selector } from './selector';
 export { default as transform } from './transform';
